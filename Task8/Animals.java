f2213f371be8b13ef29a0a415425d5c914e7c1d5
AlinaArtemchuk <artemchukalina544@gmail.com>
2022-10-24 18:03:45 +0300

    add new exeption class and new exeption

--- Task8/Animals.java
+++ Task8/Animals.java
@@ -29,8 +29,31 @@ public class Animals {
                     try {
                         String AnimalsName4 = AnimalsName.get(3);
                     } catch (IndexOutOfBoundsException e) {
-                System.err.println("Wrong animal`s number");
-                    }
-    }}
+                System.err.println("Wrong animal`s name");
+
+        }
+        class MyExeption {
+            class InvalidNumberExeptions extends Exception {
+                public InvalidNumberExeptions(String message)
+                {
+                    super (message);
+                }
+                public InvalidNumberExeptions() {
+                }
+                class NumberExeptions{
+                    public void checkNumber(int num) throws InvalidNumberExeptions  {
+                        if (num >3)
+                            throw new InvalidNumberExeptions ("Number is too big");
+                        else if (num <1) {
+                            throw new InvalidNumberExeptions("Number is too small");
+                        }
+                        try {
+                            checkNumber (4);
+                        } catch (InvalidNumberExeptions e) {
+                            System.err.println("Number is too big" + e);
+                        }
+                    }}}
+
+        }}}
 
 
